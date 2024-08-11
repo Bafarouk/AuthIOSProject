621dce653721cdb3b05051a8ea1c37e686f91a41
medfarouk <med.faroukakacha@gmail.com>
2024-08-11 02:53:22 +0100

    Initial Commit

---
+++ AuthIOSProject/AuthIOSProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthIOSProjectApp.swift
+//  AuthIOSProject
+//
+//  Created by Mohamed Farouk Ben Akacha on 11/8/2024.
+//
+
+import SwiftUI
+
+@main
+struct AuthIOSProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AuthIOSProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AuthIOSProject
+//
+//  Created by Mohamed Farouk Ben Akacha on 11/8/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
